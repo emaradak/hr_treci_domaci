ce7f303769938dfd407f0a0f056dfa0c3740f786
Ema Radak <emaradak3@gmail.com>
2023-09-05 20:31:23 +0200

    stranica o meni

--- src/stranice/About.js
+++ src/stranice/About.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {Col, Container, Image, Row} from "react-bootstrap";
+import ja from "../ja.jpeg";
 
 const About = () => {
     return (
         <>
-            <h1>O meni</h1>
+            <Container>
+                <h1>O meni</h1>
+                <Row>
+                    <Col>
+                        <Image src={ja} roundedCircle />
+
+                    </Col>
+                    <Col>
+                        <p>
+                            Ovaj zadatak je uradjen u sklopu predmeta <strong>Internet tehnologije</strong> na <strong>Fakultetu organizacionih nauka</strong>.
+                            Aplikacija je uradjena u <strong>React</strong> biblioteci.
+                            Prikazuje radnika, njegove podatke i projekte na kojima radi.
+                            Moguce je dodati radnika u projekat i izbaciti radnika iz projekta.
+                        </p>
+                    </Col>
+                </Row>
+
+            </Container>
         </>
     );
 };
